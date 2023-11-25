18bfba96e5debc27cbcd20a68ef60258b9d55cba
saipranavrguduru <116751855+saipranavrguduru@users.noreply.github.com>
2023-11-24 21:28:28 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/dsaproject3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dsaproject3.dir/main.cpp.o"
+  "CMakeFiles/dsaproject3.dir/main.cpp.o.d"
+  "dsaproject3"
+  "dsaproject3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dsaproject3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
